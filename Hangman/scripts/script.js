1d5cfa2e157bdb92e2e77bab77b3cfc50e452ff6
Jonas Andre <110419848+JonasAJohansen@users.noreply.github.com>
2024-08-19 23:40:49 +0200

    Currently making the Hints, and words

--- Hangman/scripts/script.js
+++ Hangman/scripts/script.js
@@ -1,9 +1,22 @@
-const keyboardDiv = document.querySelector(".keyboard")
+const wordDisplay = document.querySelector(".word-display");
+const keyboardDiv = document.querySelector(".keyboard");
 
-//Keyboard Buttons
-for (let i = 97; i <= 122; i++) {
-    const button = document.createElement('button');
-    button.innerText = String.fromCharCode(i);
-    keyboardDiv.appendChild(button);
-
+const getRandomWord = () => {
+    //Selecting a random word
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word);
+    document.querySelector(".hint-text b").innerText = hint;
+    wordDisplay.innerHTML = word.split("").map(() => '<li class="letter"></li>').join("");
 }
+
+// Ensuring that the DOM is fully loaded before manipulating it
+document.addEventListener('DOMContentLoaded', () => {
+    // Keyboard Buttons
+    for (let i = 97; i <= 122; i++) {
+        const button = document.createElement('button');
+        button.innerText = String.fromCharCode(i);
+        keyboardDiv.appendChild(button);
+    }
+
+    getRandomWord();
+});
---
+++ Hangman/scripts/word-list.js
@@ -0,0 +1,27 @@
+const wordList  = [
+    {
+        word: 'fbæsj1',
+        hint: 'u gay'
+    },
+    {
+        word: 'fbæsj2131',
+        hint: 'u gay'
+    },
+    {
+        word: 'fbæsj1321',
+        hint: 'u gay'
+    },
+    {
+        word: 'fbæsj123',
+        hint: 'u gay'
+    },
+
+    {
+        word: 'fbæsj511',
+        hint: 'u gay'
+    },
+    {
+        word: 'fbæsj1555',
+        hint: 'u gay'
+    },
+]
